250b854f726de948377ceb658bc7b3491df270da
Nikki A <nikki.apostolakis@gmail.com>
2019-01-29 23:53:46 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,5 +12,21 @@ class RecipesController < ApplicationController
   end
 
   def create
+    Recipe.create(recipe_params)
+    redirect_to recipe_path
   end
+
+  
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(
+    :title,
+    ingredients_attributes: [
+      :name,
+      :quantity
+    ]
+  )
+  end
+  
 end
