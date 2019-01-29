93b907aa198505568967ba55eef29eb50e8e3ead
Nikki A <nikki.apostolakis@gmail.com>
2019-01-29 23:57:28 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,8 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
-    @recipe.ingredients.build()
-    @recipe.ingredients.build()
+    @recipe.ingredients.build(name: " ")
+    @recipe.ingredients.build(name: " ")
   end
 
   def create
